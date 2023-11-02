231218cb9bda86efd0dd91143b06773b567e7cab
Tonie Victor <victornwanochi@gmail.com>
2023-11-01 17:12:25 -0700

    Print ASCII characters in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase, not followed by a new line."""
-
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for number in range(97, 123):
+	print(f"{chr(number):s}", end="")
