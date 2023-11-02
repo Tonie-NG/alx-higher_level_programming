02f62404bc47b01fb35c327285a1671e482aacb3
Tonie Victor <victornwanochi@gmail.com>
2023-11-02 16:28:42 -0700

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,7 @@
 if __name__ == "__main__":
     from sys import argv
 
-    no_of_args = len(argv) - 1
-    if no_of_args == 0:
-        print("{}".format(no_of_args))
-    elif no_of_args == 1:
-        print("{}".format(argv[no_of_args]))
-    else:
-        c = argv[1]
-        for i in range(2, len(argv)):
-            c += int(argv[i])
-        print("{}".format(c))
+    result = 0
+    for i in range(1, len(argv)):
+        result += int(argv[i])
+    print("{}".format(result))
