0d8cc0d11c082b1d37d07908d3d9f08c3f76fb0f
Tonie Victor <victornwanochi@gmail.com>
2023-11-03 23:46:30 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 def no_c(my_string):
-    """
-    A function that removes all occurrence oif C from a string
-    """
-    new_string = ""
-    for char in my_string:
-        if char != "c" and char != "C":
-            new_string += char
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return new_string
-
