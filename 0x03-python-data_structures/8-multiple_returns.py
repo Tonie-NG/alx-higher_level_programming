a3dfee9e511287b39e89290304e89a100e822c4d
Tonie Victor <victornwanochi@gmail.com>
2023-11-05 15:19:56 -0800

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,9 @@
 
 def multiple_returns(sentence):
     sen_len = len(sentence)
-    if len(sen_len) == 0:
-        return None
-    return ("Length: {} - First character: {}".format(sen_len, sentence[0]))
+    if sen_len == 0:
+        result = (sen_len, None)
+    else:
+        result = (sen_len, sentence[0])
+
+    return result
