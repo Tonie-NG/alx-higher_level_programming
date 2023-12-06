47dd0c4d0eb789887c5fac271faa8ada2b6f0d6b
Tonie Victor <victornwanochi@gmail.com>
2023-12-05 16:12:46 -0800

    I forgot to make it executable :)

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,10 +11,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
     try:
-        item_list = load_from_json_file("add_item.json")
+        item = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        item_list = []
+        item = []
 
-    item_list.extend(sys.argv[1:])
+    item.extend(sys.argv[1:])
 
-    save_to_json_file(item_list, "add_item.json")
+    save_to_json_file(item, "add_item.json")
