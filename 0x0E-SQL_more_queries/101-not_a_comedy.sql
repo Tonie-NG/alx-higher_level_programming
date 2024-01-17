3fd56f9654a725f75272e97dc54f3403ce1373bf
Tonie Victor <victornwanochi@gmail.com>
2024-01-17 14:34:45 -0800

    fix: No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,7 +8,8 @@ WHERE title not in (
 	LEFT JOIN tv_show_genres
 	ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres
-	ON tv_show_genres.genre_id = tv_genres.i
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy'
 )
 GROUP BY title
 ORDER BY title ASC;
