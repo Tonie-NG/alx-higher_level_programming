263375832b8098fe5e5b1930070b456547e51f40
Tonie Victor <victornwanochi@gmail.com>
2024-03-01 20:18:02 -0800

    What's my status? #1

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# sends a request to the URL and displays the body of the response
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code:', err.code)
---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# fetches https://alx-intranet.hbtn.io/status
+
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get("https://intranet.hbtn.io/status")
+    res = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
