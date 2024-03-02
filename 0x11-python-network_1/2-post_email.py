793bd6481bbd6484aa9119fb27e3ec73d59bbd6b
Tonie Victor <victornwanochi@gmail.com>
2024-03-01 19:48:53 -0800

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# sends a POST request to the passed URL with the email as a parameter
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import parse, request
+
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = parse.urlencode(value).encode("ascii")
+    req = request.Request(url, data)
+    with request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
