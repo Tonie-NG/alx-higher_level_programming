0f8fa5753ac3ee9b9f051e41a995b8438c7779c9
Tonie Victor <victornwanochi@gmail.com>
2024-03-04 02:40:54 -0800

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) 
+of the repository “rails” by the user “rails”
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    giturl = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2],
+        sys.argv[1]
+        )
+    req = requests.get(giturl)
+    jsonlist = req.json()
+
+    for i in range(10):
+        print("{}: {}".format(
+            jsonlist[i].get("sha"),
+            jsonlist[i].get("commit").get("author").get("name")))
